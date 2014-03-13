34e7d6321a3a49e0eeb5c462e37afb23a5a4b8a0
Cory Forsyth <cory.forsyth@gmail.com>
2014-03-12 20:32:06 -0400

    first

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+var server = http.createServer(function(req, res){
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.end('<h1>Hello World</h1>');
+});
+
+var port = Number(process.env.PORT || 5000);
+server.listen(3000, 'localhost');
