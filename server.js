de05a05a924d801fbc90cc7c772edc58ad60fa30
Cory Forsyth <cory.forsyth@gmail.com>
2014-03-12 20:33:18 -0400

    log

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var http = require('http');
 
+console.log('Starting server');
+
 var server = http.createServer(function(req, res){
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.end('<h1>Hello World</h1>');
