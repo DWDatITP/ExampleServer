c166e5d73878b96bf86ddf852f424f4f45da3b62
Cory Forsyth <cory.forsyth@gmail.com>
2014-04-09 10:22:25 -0400

    Move over to using views, add stub data

---
+++ data.js
@@ -0,0 +1,7 @@
+module.exports = {
+  students: [
+    {name: 'Student 1', attendance: ['Y', 'Y', 'Y', 'N', 'N', 'N', 'N']},
+    {name: 'Student 2', attendance: ['Y', 'Y', 'N', 'N', 'N', 'N', 'N']},
+    {name: 'Student 3', attendance: ['Y', 'N', 'Y', 'N', 'N', 'N', 'N']}
+  ]	
+};
--- server.js
+++ server.js
@@ -4,12 +4,31 @@ var expressHandlebars = require('express3-handlebars');
 var app = express();
 var handlebars = expressHandlebars.create({defaultLayout: 'main'});
 
+var data = require('./data');
+
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 app.get('/', function(req, res){
-  res.render('index', {luckyNumber: Math.round( Math.random() * 10 )})
+  res.render('index');
 });
+
+app.get('/students', function(req, res){
+  res.render('students', {
+    students: data.students
+  });
+});
+
+app.get('/weeks', function(req, res){
+  res.render('weeks', {
+    weekNumbers: [1,2,3,4,5,6,7]
+  });
+});
+
+app.get('/weeks/:weekNumber', function(req, res){
+  var weekNumber = req.params.weekNumber;
+  res.render('week', {weekNumber: weekNumber});
+})
 
 app.use('/public', express.static('public'));
 
